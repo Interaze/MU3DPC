4cbedb77f8834bde97692bb1079965d39d61edfe
Christopher Scully <scullycphone@gmail.com>
2020-02-01 15:51:05 -0600

    initial modeling

---
+++ index.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <title>Home</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="A free toolkit for Developing and Testing different algorithms.">
+    <meta name="keywords" content="Graph Building,Algorithms,SandBox,D3.js,HTML,CSS,JS">
+    <meta name="author" content="Christopher Scully">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="StyleSheets/theme.css">
+    <link rel="stylesheet" href="StyleSheets/cusStyle.css">
+
+</head>
+<body>
+    <?php include ('navBar.html'); ?>
+
+    <div class="bg-secondary row d-flex m-4">
+        <div class="col-lg-5 col-sm-6 col-xs-12 h-100 align-self-center border-right border-primary pl-5 pr-4 pt-4 pb-4">
+            <h1>
+                A club focused on innovation and
+            </h1>
+            <button type="button" class="btn btn-primary mt-2">Learn More</button>
+        </div>
+        <img src="https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/42880975_1116623265153666_6510657429842165760_o.jpg?_nc_cat=108&_nc_ohc=T91yxD6Ma-YAX8e_iGi&_nc_ht=scontent-den4-1.xx&oh=d44790503663fe5eeea65097a83ec869&oe=5EBF8AAC" class="col-lg-7 col-sm-6 col-xs-12 mh-100 nodrag pr-0">
+    </div>
+
+
+    <?php include ('footer.html'); ?>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>

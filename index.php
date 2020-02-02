a5ff2578f19fe1b5c049ec66a0d1890828359caa
Christopher Scully <scullycphone@gmail.com>
2020-02-01 23:47:31 -0600

    added anchor directory and wired additional sections

--- index.php
+++ index.php
@@ -40,43 +40,115 @@
         <div class="col-lg-5 col-md-6 col-sm-12 h-100 align-self-center pr-sm-0 pr-md-4 order-md-1 order-sm-2 px-0">
             <div class="border-right border-sm-0 border-danger px-sm-0 px-md-5 py-4 pl-xl-0">
                 <h1 class="autoscale">
-                    A Club Dedicated to Learning, Innovation, and Growth
+                    A Club Dedicated to Education, Innovation, and Growth
                 </h1>
-                <button type="button" class="btn btn-outline-danger mt-2">Learn More</button>
+                <a href="#about"><button type="button" class="btn btn-outline-danger mt-2">Learn More</button></a>
             </div>
         </div>
 
     </div>
-    <div class="row bg-secondary col-12 m-0 px-5 my-5 d-block">
+    <div class="container">
+        <h1 class="text-primary ">Recent Events</h1>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a class="anchor" id="about"></a>
+    <div class="row bg-secondary m-0 px-4 my-5 d-block col-md-12 col-lg-9 mx-auto">
         <h1 class="text-center d-block">What We Do</h1>
         <br>
         <div class="d-md-flex d-sm-block justify-content-center">
-            <div class="card border-0 col-md-3 col-sm-12 mx-sm-0 mx-md-3 my-sm-3 my-md-0">
-                <i class="fas fa-hand-holding-heart text-center"></i>
-                <div class="card-body">
-                  <h5 class="card-title">Card title that wraps to a new line</h5>
+            <div class="card border-0 col-md-3 col-sm-12 mx-sm-0 mx-md-2 my-sm-3 my-md-0">
+                <i class="fas fa-university text-center tricon" style="color: forestgreen"></i>
+                <div class="card-body px-0">
+                    <h3 class="card-title text-center">Education</h5>
                 </div>
             </div>
-            <div class="card border-0 col-md-3 col-sm-12 mx-sm-0 mx-md-3 my-md-0 my-sm-3">
-                <i class="fas fa-hand-holding-heart text-center"></i>
-                <div class="card-body">
-                <h5 class="card-title">Card title that wraps to a new line</h5>
+            <div class="card border-0 col-md-3 col-sm-12 mx-sm-0 mx-md-2 my-md-0 my-sm-3">
+                <i class="fas fa-vial text-center tricon" style="color: royalblue"></i>
+                <div class="card-body px-0">
+                    <h3 class="card-title text-center">Research</h5>
                 </div>
             </div>
-            <div class="card border-0 col-md-3 col-sm-12 mx-sm-0 mx-md-3 my-md-0 my-sm-3">
-                <i class="fas fa-hand-holding-heart text-center"></i>
-                <div class="card-body">
-                <h5 class="card-title">Card title that wraps to a new line</h5>
+            <div class="card border-0 col-md-3 col-sm-12 mx-sm-0 mx-md-2 my-md-0 my-sm-3">
+                <i class="fas fa-hand-holding-heart text-center tricon" style="color: #ff2154"></i>
+                <div class="card-body px-0">
+                    <h3 class="card-title text-center">Community</h5>
                 </div>
             </div>
         </div>
-
     </div>
-    <div class="row bg-light col-12 m-0" style="height: 300px">
-        <h1 class="text-primary">About Us:</h1>
+    <div class="row col-12 m-0" style="height: 300px">
+        <h1 class="text-primary ">Our Focus:</h1>
     </div>
-    <div class="row bg-light col-12 m-0" style="height: 300px">
-        <h1 class="text-primary">About Us:</h1>
+    <div class="row col-12 m-0" style="height: 300px">
+        <h1 class="text-primary">Education:</h1>
+        <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Success card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+    <div class="row col-12 m-0" style="height: 300px">
+        <h1 class="text-primary">Research:</h1>
+        <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Success card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+    </div>
+    <div class="row col-12 m-0" style="height: 300px">
+        <h1 class="text-primary">Community:</h1>
+        <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Success card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
     </div>
     <?php include ('footer.html'); ?>
 

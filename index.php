83f0926b75b945e894d7bb875ab2051f6c442096
Christopher Scully <scullycphone@gmail.com>
2020-02-01 22:23:13 -0600

    forgot to push, new spacing,styles, etc

--- index.php
+++ index.php
@@ -16,21 +16,73 @@
 <body>
     <?php include ('navBar.html'); ?>
 
-    <div class="bg-secondary row d-flex m-4">
-        <div class="col-lg-5 col-sm-6 col-xs-12 h-100 align-self-center border-right border-primary pl-5 pr-4 pt-4 pb-4">
-            <h1>
-                A club focused on innovation and
-            </h1>
-            <button type="button" class="btn btn-primary mt-2">Learn More</button>
+    <div class="bg-secondary row d-flex my-md-5 mx-sm-0 mx-md-4 mx-xl-auto my-sm-0 col-xl-10 mx-0 px-0">
+        <div class="imgFrame align-self-center col-lg-7 col-md-6 col-sm-12 p-0 order-md-2 order-sm-1">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100 nodrag " src="https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/42880975_1116623265153666_6510657429842165760_o.jpg?_nc_cat=108&_nc_ohc=T91yxD6Ma-YAX8e_iGi&_nc_ht=scontent-den4-1.xx&oh=d44790503663fe5eeea65097a83ec869&oe=5EBF8AAC" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 nodrag " src="https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/42880975_1116623265153666_6510657429842165760_o.jpg?_nc_cat=108&_nc_ohc=T91yxD6Ma-YAX8e_iGi&_nc_ht=scontent-den4-1.xx&oh=d44790503663fe5eeea65097a83ec869&oe=5EBF8AAC" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 nodrag " src="https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/42880975_1116623265153666_6510657429842165760_o.jpg?_nc_cat=108&_nc_ohc=T91yxD6Ma-YAX8e_iGi&_nc_ht=scontent-den4-1.xx&oh=d44790503663fe5eeea65097a83ec869&oe=5EBF8AAC" alt="Third slide">
+                </div>
+            </div>
+            </div>
         </div>
-        <img src="https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/42880975_1116623265153666_6510657429842165760_o.jpg?_nc_cat=108&_nc_ohc=T91yxD6Ma-YAX8e_iGi&_nc_ht=scontent-den4-1.xx&oh=d44790503663fe5eeea65097a83ec869&oe=5EBF8AAC" class="col-lg-7 col-sm-6 col-xs-12 mh-100 nodrag pr-0">
+        <div class="col-lg-5 col-md-6 col-sm-12 h-100 align-self-center pr-sm-0 pr-md-4 order-md-1 order-sm-2 px-0">
+            <div class="border-right border-sm-0 border-danger px-sm-0 px-md-5 py-4 pl-xl-0">
+                <h1 class="autoscale">
+                    A Club Dedicated to Learning, Innovation, and Growth
+                </h1>
+                <button type="button" class="btn btn-outline-danger mt-2">Learn More</button>
+            </div>
+        </div>
+
     </div>
+    <div class="row bg-secondary col-12 m-0 px-5 my-5 d-block">
+        <h1 class="text-center d-block">What We Do</h1>
+        <br>
+        <div class="d-md-flex d-sm-block justify-content-center">
+            <div class="card border-0 col-md-3 col-sm-12 mx-sm-0 mx-md-3 my-sm-3 my-md-0">
+                <i class="fas fa-hand-holding-heart text-center"></i>
+                <div class="card-body">
+                  <h5 class="card-title">Card title that wraps to a new line</h5>
+                </div>
+            </div>
+            <div class="card border-0 col-md-3 col-sm-12 mx-sm-0 mx-md-3 my-md-0 my-sm-3">
+                <i class="fas fa-hand-holding-heart text-center"></i>
+                <div class="card-body">
+                <h5 class="card-title">Card title that wraps to a new line</h5>
+                </div>
+            </div>
+            <div class="card border-0 col-md-3 col-sm-12 mx-sm-0 mx-md-3 my-md-0 my-sm-3">
+                <i class="fas fa-hand-holding-heart text-center"></i>
+                <div class="card-body">
+                <h5 class="card-title">Card title that wraps to a new line</h5>
+                </div>
+            </div>
+        </div>
 
-
+    </div>
+    <div class="row bg-light col-12 m-0" style="height: 300px">
+        <h1 class="text-primary">About Us:</h1>
+    </div>
+    <div class="row bg-light col-12 m-0" style="height: 300px">
+        <h1 class="text-primary">About Us:</h1>
+    </div>
     <?php include ('footer.html'); ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+    </script>
 </body>
 </html>

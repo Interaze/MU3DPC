e9d168c80bef246e3dec87308a4796c8fc5357cd
jdpeiffer6 <jdp6n8@mail.missouri.edu>
2020-03-24 17:50:47 -0500

    added pictures and people page

--- FrontEnd/Events.php
+++ FrontEnd/Events.php
@@ -62,8 +62,7 @@
         </div>
     </div>
 
-
-    <?php include ('footer.html'); ?>
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FChicago&amp;src=bXUzZHBjQGdtYWlsLmNvbQ&amp;color=%23039BE5" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
---
+++ FrontEnd/People.php
@@ -0,0 +1,105 @@
+<html lang="en">
+<head>
+    <title>Home</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="A free toolkit for Developing and Testing different algorithms.">
+    <meta name="keywords" content="Graph Building,Algorithms,SandBox,D3.js,HTML,CSS,JS">
+    <meta name="author" content="J.D. Peiffer">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="StyleSheets/theme.css">
+    <link rel="stylesheet" href="StyleSheets/cusStyle.css">
+
+</head>
+<body>
+    <?php include ('navBar.html'); ?>
+    <div class="container mt-5">
+        <h1>People</h1>
+    </div>
+<div class="row container mt-1 mx-auto d-flex">
+ <div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
+            <div class="col-12 p-0">
+                <div class="projects m-0">
+                    <img src="images/andrew1.jpg" class="w-100 projImg">
+                    <div class="caption">
+					<h2 class="my-auto">Andrew Dove</h2>
+                        <button class="btn btn-danger mx-auto my-auto">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+		
+		   <div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
+            <div class="col-12 p-0">
+                <div class="projects m-0">
+                    <img src="images/anthony.jfif" class="w-100 projImg">
+                    <div class="caption">
+					<h2 class="my-auto">Anthony Hughes</h2>
+                        <button class="btn btn-danger mx-auto my-auto">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+		
+
+        <div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
+            <div class="col-12 p-0">
+                <div class="projects m-0">
+                    <img src="images/jackallen-1.jpg" class="w-100 projImg">
+                    <div class="caption">
+                        <h2 class="my-auto">Jack Allen</h2>
+                        <button class="btn btn-danger mx-auto my-auto">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+		
+        <div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
+            <div class="col-12 p-0">
+                <div class="projects m-0">
+                    <img src="images/chris1.jpg" class="w-100 projImg">
+                    <div class="caption">
+					<h2 class="my-auto">Rev. Chris Scully</h2>
+                        <button class="btn btn-danger mx-auto my-auto">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+		
+		<div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
+            <div class="col-12 p-0">
+                <div class="projects m-0">
+                    <img src="images/jd1.jpg" class="w-100 projImg">
+                    <div class="caption">
+					<h2 class="my-auto">J.D. Peiffer</h2>
+                        <button class="btn btn-danger mx-auto my-auto">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+		
+		<div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
+            <div class="col-12 p-0">
+                <div class="projects m-0">
+                    <img src="images/chris1.jpg" class="w-100 projImg">
+                    <div class="caption">
+					<h2 class="my-auto">Paul Kraus</h2>
+                        <button class="btn btn-danger mx-auto my-auto">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+     
+
+
+    </div>
+	
+	<?php include ('footer.html'); ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>

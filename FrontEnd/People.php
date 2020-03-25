261049ccf0550c143a2ccbfb3a5997e9b80ff29f
Christopher Scully <scullycphone@gmail.com>
2020-03-25 02:40:05 -0500

    updated important info

--- FrontEnd/People.php
+++ FrontEnd/People.php
@@ -18,8 +18,8 @@
     <div class="container mt-5">
         <h1>People</h1>
     </div>
-	
-	
+
+
 	<div class="container border-left border-info px-5">
         <h1 class="text-primary ">Officers</h1>
         <div class="row no-gutters">
@@ -48,7 +48,7 @@
                         <div class="col-md-8">
                             <div class="card-body">
                                 <h5 class="card-title">Hnthony Aughes</h5>
-								<p class="card-text"><small class="text-muted">Baller</small></p>
+								<p class="card-text"><small class="text-muted">Karate Master</small></p>
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             </div>
                         </div>
@@ -64,7 +64,7 @@
                         <div class="col-md-8">
                             <div class="card-body">
                                 <h5 class="card-title">Shris Ccully</h5>
-								<p class="card-text"><small class="text-muted">Karate Master</small></p>
+								<p class="card-text"><small class="text-muted">Rev. Cash Money</small></p>
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             </div>
                         </div>
@@ -81,7 +81,7 @@
                             <div class="card-body">
                                 <h5 class="card-title">Rose Schuffler</h5>
 								<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>                               
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             </div>
                         </div>
                     </div>
@@ -99,7 +99,7 @@
                             <div class="card-body">
                                 <h5 class="card-title">Jack Allen</h5>
 								<p class="card-text"><small class="text-muted">Big brain</small></p>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>                               
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             </div>
                         </div>
                     </div>
@@ -107,7 +107,7 @@
             </div>
         </div>
     </div>
-	
+
 	<div class="container border-left border-info px-5">
         <h1 class="text-primary ">Project Leads</h1>
         <div class="row no-gutters">
@@ -159,7 +159,7 @@
                     </div>
                 </div>
             </div>
-   
+
 	</div>
 	<!--
 <div class="row container mt-1 mx-auto d-flex">
@@ -174,7 +174,7 @@
                 </div>
             </div>
         </div>
-		
+
 		   <div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
             <div class="col-12 p-0">
                 <div class="projects m-0">
@@ -186,7 +186,7 @@
                 </div>
             </div>
         </div>
-		
+
 
         <div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
             <div class="col-12 p-0">
@@ -199,7 +199,7 @@
                 </div>
             </div>
         </div>
-		
+
         <div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
             <div class="col-12 p-0">
                 <div class="projects m-0">
@@ -211,7 +211,7 @@
                 </div>
             </div>
         </div>
-		
+
 		<div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
             <div class="col-12 p-0">
                 <div class="projects m-0">
@@ -223,7 +223,7 @@
                 </div>
             </div>
         </div>
-		
+
 		<div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
             <div class="col-12 p-0">
                 <div class="projects m-0">
@@ -235,7 +235,7 @@
                 </div>
             </div>
         </div>
-		
+
 		<div class="col-lg-6 col-md-12 mb-1 px-3 py-3">
             <div class="col-12 p-0">
                 <div class="projects m-0">
@@ -247,7 +247,7 @@
                 </div>
             </div>
         </div>
-     
+
 
 
     </div>

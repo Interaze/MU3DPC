2c01b710aed9243afc8d1fddf941e329a2bee16b
Christopher Scully <scullycphone@gmail.com>
2020-04-07 22:00:07 -0500

    Update index.php

--- FrontEnd/index.php
+++ FrontEnd/index.php
@@ -247,7 +247,7 @@
         <?php include ('footer.html'); ?>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.buttflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     </body>
 </html>
